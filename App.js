47f5260df19d76d30bac4da85e274fb77e3a5336
philipp <philipp.reiter90@gmail.com>
2023-09-28 11:46:56 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { ForceTrimMode, Tool, VESDK, VideoCodec, VideoFormat } from 'react-native-videoeditorsdk';
 
 export default function App() {
+  const openVideoFromCameraRollExample = async () => {
+    try {
+      // Select a video from the camera roll.
+      let pickerResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      });
+
+      // Return if the video selection has been cancelled.
+      if (pickerResult.canceled) {
+        return;
+      }
+
+      // Open the video editor and handle the export as well as any occuring errors.
+      const result = await VESDK.openEditor(pickerResult.assets[0].uri,
+        {
+          tools: [Tool.COMPOSITION, Tool.TRIM, Tool.TRANSFORM, Tool.STICKER, Tool.TEXT],
+          trim: {maximumDuration: 14, forceMode: ForceTrimMode.ALWAYS},
+          theme: 'light',
+          export: {video: {format: VideoFormat.MP4, codec: VideoCodec.H264}},
+          transform: {
+            items: [{height: 16, width: 9, name: '16:9', toggleable: false}],
+            allowFreeCrop: false,
+          },
+        })
+
+      if (result != null) {
+        // The user exported a new video successfully and the newly generated video is located at `result.video`.
+        console.log(result?.video);
+      } else {
+        // The user tapped on the cancel button within the editor.
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Open Video Editor" onPress={openVideoFromCameraRollExample}/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
